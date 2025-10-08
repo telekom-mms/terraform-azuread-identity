ec7244653b966ef1096bac92391eecda49f1d403
renandeud <renan.deud@t-systems.com>
2025-10-08 09:40:55 -0300

    feat: upgrade-azurerm-provider

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=3.6, <4.0"
+      version = ">=4.40.0, <5.0"
     }
     azuread = {
       source  = "registry.terraform.io/hashicorp/azuread"
-      version = ">=4.40.0, <5.0"
+      version = ">=2.22, <3.0"
     }
   }
   required_version = ">=1.0"
